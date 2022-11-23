05e50b5e04b9b64b90e31b8e36fcaca40bb6748d
¡Duque <atanaelleonardo@gmail.com>
2022-11-22 23:46:07 -0300

    𝑨𝒖𝒍𝒂 02 𝒄𝒐𝒎𝒑𝒍𝒆𝒕𝒂 ✔

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,25 +1,19 @@
-import {
-  Roboto_400Regular,
-  Roboto_500Medium,
-  Roboto_700Bold,
-  useFonts,
-} from "@expo-google-fonts/roboto";
-import { NativeBaseProvider, StatusBar } from "native-base";
-import { Loading } from "./src/components/Loading";
-import { SignIn } from "./src/screens/Signin";
-import { THEME } from "./src/styles/theme";
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
+
+import { THEME } from './src/styles/theme';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_500Medium,
-    Roboto_700Bold,
-  });
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
+      <StatusBar 
+        barStyle='light-content'
         backgroundColor="transparent"
         translucent
       />
