f07bf8ab0629715b5242f512ba1242e81abc2d39
¡Duque <atanaelleonardo@gmail.com>
2022-11-26 14:02:31 -0300

    𝑹𝒆𝒅𝒊𝒓𝒆𝒄𝒊𝒐𝒏𝒂𝒎𝒆𝒏𝒕𝒐 𝒅𝒐 𝒖𝒔𝒖𝒂𝒓𝒊𝒐 𝒑𝒂𝒓𝒂 𝒓𝒐𝒕𝒂𝒔 𝒅𝒂 𝒂𝒑𝒍𝒊𝒄𝒂𝒄𝒂𝒐

--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
 import { Box, HStack, Text } from "native-base";
 import { CaretLeft, Export } from "phosphor-react-native";
+
 import { ButtonIcon } from "../ButtonIcon";
 
 interface Props {
--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useTheme } from "native-base";
 import { PlusCircle, SoccerBall } from "phosphor-react-native";
 import { Platform } from "react-native";
+
 import { Find } from "../screens/Find";
 import { New } from "../screens/New";
 import { Pools } from "../screens/Pools";
@@ -10,7 +11,9 @@ const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
   const { colors, sizes } = useTheme();
+
   const size = sizes[6];
+
   return (
     <Navigator
       screenOptions={{
@@ -20,7 +23,7 @@ export function AppRoutes() {
         tabBarInactiveTintColor: colors.gray[300],
         tabBarStyle: {
           position: "absolute",
-          height: sizes[20],
+          height: sizes[22],
           borderTopWidth: 0,
           backgroundColor: colors.gray[800],
         },
@@ -31,7 +34,7 @@ export function AppRoutes() {
       }}
     >
       <Screen
-        name={"new"}
+        name="new"
         component={New}
         options={{
           tabBarIcon: ({ color }) => <PlusCircle color={color} size={size} />,
@@ -39,7 +42,7 @@ export function AppRoutes() {
         }}
       />
       <Screen
-        name={"pools"}
+        name="pools"
         component={Pools}
         options={{
           tabBarIcon: ({ color }) => <SoccerBall color={color} size={size} />,
@@ -48,11 +51,9 @@ export function AppRoutes() {
       />
 
       <Screen
-        name={"find"}
+        name="find"
         component={Find}
-        options={{
-          tabBarButton: () => null,
-        }}
+        options={{ tabBarButton: () => null }}
       />
     </Navigator>
   );
