9f63e52a8f298f10c3dd2f262d317b4ce9df1fda
¡Duque <atanaelleonardo@gmail.com>
2022-11-26 11:14:10 -0300

    𝑪𝒓𝒊𝒂𝒄𝒂𝒐 𝒅𝒆 𝒖𝒔𝒖𝒂𝒓𝒊𝒐 𝒆 𝒈𝒆𝒓𝒂𝒄𝒂𝒐 𝒅𝒆 𝒋𝒘𝒕

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
 import Fastify from "fastify";
+
 import { authRoutes } from "./routes/Auth";
 import { gameRoutes } from "./routes/Game";
 import { guessRoutes } from "./routes/Guess";
@@ -15,14 +17,19 @@ async function bootstrap() {
     origin: true,
   });
 
+  // Em produção isso precisa ser uma viarável de ambiente
+
+  await fastify.register(jwt, {
+    secret: "nlwcopa",
+  });
+
   await fastify.register(poolRoutes);
   await fastify.register(authRoutes);
   await fastify.register(gameRoutes);
   await fastify.register(guessRoutes);
   await fastify.register(userRoutes);
 
-  const port = 3333;
-  await fastify.listen({ port: port /*host: "0.0.0.0"*/ });
+  await fastify.listen({ port: 3333, host: "0.0.0.0" });
 }
 
 bootstrap();
