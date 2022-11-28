a9e4e076b4184da48e6317feb3973cafcea4a8d5
¡Duque <atanaelleonardo@gmail.com>
2022-11-27 22:54:24 -0300

    𝑪𝒓𝒊𝒂𝒏𝒅𝒐 𝒏𝒐𝒗𝒐 𝒃𝒐𝒍𝒂𝒐

--- mobile/src/screens/New/index.tsx
+++ mobile/src/screens/New/index.tsx
@@ -1,10 +1,49 @@
-import { Heading, Text, VStack } from "native-base";
+import { Heading, Text, useToast, VStack } from "native-base";
+import { useState } from "react";
 import Logo from "../../assets/logo.svg";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
+import { api } from "../../services/api";
 
 export function New() {
+  const [title, setTitle] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+
+  async function handlePollCreate(params: any) {
+    if (!title.trim()) {
+      return toast.show({
+        title: "Informe o nome para o vosso bolão",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
+
+    try {
+      setIsLoading(true);
+
+      await api.post("/pools", { title });
+
+      toast.show({
+        title: `Bolão "${title}" criado com sucesso`,
+        placement: "top",
+        bgColor: "green.500",
+      });
+
+      setTitle("");
+    } catch (error) {
+      console.error(error);
+
+      toast.show({
+        title: "Não foi possível criar o vosso bolão",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Criar novo bolão" />
@@ -22,9 +61,18 @@ export function New() {
           Crie seu próprio bolão da copa {"\n"} e compartilhe entre amigos!
         </Heading>
 
-        <Input mb={2} placeholder={"Qual o nome do vosso bolão?"} />
+        <Input
+          mb={2}
+          placeholder={"Qual o nome do vosso bolão?"}
+          onChangeText={setTitle}
+          value={title}
+        />
 
-        <Button title={"CRIAR MEU BOLÃO"} />
+        <Button
+          title={"CRIAR MEU BOLÃO"}
+          onPress={handlePollCreate}
+          isLoading={isLoading}
+        />
 
         <Text
           color={"gray.200"}
