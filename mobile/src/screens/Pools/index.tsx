d863755a005164915b24e71e9a84a901a6d7a22c
¡Duque <atanaelleonardo@gmail.com>
2022-11-27 23:03:25 -0300

    𝑳𝒊𝒔𝒕𝒂𝒏𝒅𝒐 𝒃𝒐𝒍𝒐𝒆𝒔 𝒄𝒓𝒊𝒂𝒅𝒐𝒔

--- mobile/src/screens/Pools/index.tsx
+++ mobile/src/screens/Pools/index.tsx
@@ -1,11 +1,26 @@
 import { Octicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { Icon, VStack } from "native-base";
+import { useEffect } from "react";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { api } from "../../services/api";
 
 export function Pools() {
   const { navigate } = useNavigation();
+
+  async function fetchPools() {
+    try {
+      const response = await api.get("/pools");
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPools();
+  }, []);
 
   return (
     <VStack flex={1} bgColor="gray.900">
